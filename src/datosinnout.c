3c8e7140b4f452ddaf3be2c8611c3e71f984882b
Pieris128 <elpichichi2048@gmail.com>
2021-12-23 19:19:33 -0300

    agrego comentarios

--- src/datosinnout.c
+++ src/datosinnout.c
@@ -9,7 +9,12 @@ int main(){
 	float b = 15.5;
 	char c = 'e';
 
-	printf("%i %.1f %c",a,b,c);
+	printf("%i %.1f %c",a,b,c); /* de esta 
+				       forma
+				       usamos 
+				       varios
+				       datos 
+				       a la vez*/
 
 	return 0;
 
