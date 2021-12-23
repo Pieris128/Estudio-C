10e1e81ba9148aefacfe96c0b947fcaa9b923763
Pieris128 <elpichichi2048@gmail.com>
2021-12-23 19:35:25 -0300

    guardo segunda parte de esta practica de entradas y salidas de datde datos

---
+++ src/4.datosinnout/datosinnout2.c
@@ -0,0 +1,18 @@
+//entradas y salidas
+
+
+#include<stdio.h>
+
+int main(){
+	int a;
+	float b;
+	char c;
+
+	printf("Digite el valor de la variable a: ");
+	scanf("%i",&a);
+
+	printf("El valor es: %i",a);
+
+	return 0;
+
+}
