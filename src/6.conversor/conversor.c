5dd463740b8d5bf633ea4300b166382156e39373
Pieris128 <elpichichi2048@gmail.com>
2021-12-25 12:43:08 -0300

    guardo conversor.c

---
+++ src/6.conversor/conversor.c
@@ -0,0 +1,18 @@
+//conversor grados celsius a farenheit
+
+
+#include<stdio.h>
+
+int main (){
+	float n, f(x) = 0;
+	
+	printf("Ingrese la temperatura en grados Celsius: ");
+	scanf("%f",&n);
+	
+	f(x)= (n*(9/5))+32
+	
+	printf("La temperatura es de: °F"
+	
+	return 0;
+
+	}
