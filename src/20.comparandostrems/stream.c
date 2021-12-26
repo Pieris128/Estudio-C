15d9420150a570e77a871b64d8cb971a00da3e31
Pieris128 <elpichichi2048@gmail.com>
2021-12-26 14:56:22 -0300

    guardo ejercicio condicionales

---
+++ src/20.comparandostrems/stream.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main (){
+	char nombre[40],sexo[20];
+	int edad;
+
+	printf("\nDigite su nombre: ");gets(nombre);
+	printf("\nDigite su sexo: ");gets(sexo);
+	if(strcmp(sexo,"masculino")==0){printf("\nDigite su edad: ");scanf("%i",&edad);
+
+		if (edad >= 18){
+		printf("Su nombre es: %s",nombre);
+	}
+	else{ 
+		printf("Usted no es un masculino mayor de edad");
+	}}
+
+			else {
+			printf("Usted no es apto");}
+
+		
+	return 0;
+}
