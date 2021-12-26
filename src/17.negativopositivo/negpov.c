ec296e3b17f5057c6cd562e16ac09775077dcfc4
Pieris128 <elpichichi2048@gmail.com>
2021-12-26 13:15:06 -0300

    guardo negpov.c tengo que consultar

---
+++ src/17.negativopositivo/negpov.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	printf("Digite un numero: ");scanf("%i",&a);
+
+	if ( a < 0 ){
+		puts("El numero es negativo");
+	}
+	if ( a > 0 ){
+		puts("El numero es positivo");
+	}
+	if ( a = 0 ){
+		printf("El numero es 0");
+	}
+	return 0;
+}
