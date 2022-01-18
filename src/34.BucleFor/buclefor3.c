12a1205158d57a36152a6845383315ca0c67fffc
Pieris128 <elpichichi2048@gmail.com>
2022-01-18 19:07:20 -0300

    escribo 100 veces no debo llegar tarde a clases con bucle for

---
+++ src/34.BucleFor/buclefor3.c
@@ -0,0 +1,13 @@
+//100 veces no debo llegar tarde
+
+#include<stdio.h>
+
+int main () {
+    int i;
+
+    for(i=1;i<=100;i++){
+        printf("No debo llegar tarde a clases\n");
+    }
+
+    return 0;
+}
