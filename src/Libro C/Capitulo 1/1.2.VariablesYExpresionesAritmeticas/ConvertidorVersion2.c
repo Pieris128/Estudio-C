e88d2ec1ffdecea6b09178889ece1578ef98f57b
Pieris128 <elpichichi2048@gmail.com>
2022-01-19 20:21:20 -0300

    Agrego encabezado

--- src/Libro C/Capitulo 1/1.2.VariablesYExpresionesAritmeticas/ConvertidorVersion2.c
+++ src/Libro C/Capitulo 1/1.2.VariablesYExpresionesAritmeticas/ConvertidorVersion2.c
@@ -9,6 +9,7 @@ lower = 0 ; /* límite superior de la tabla de temperaturas */
 upper = 300; /* límite superior */
 step = 20 ; /* tamaño del incremento */
 fahr = lower;
+printf("Tabla de conversion Farenheit a Celsius\n");
 while (fahr <= upper) {
 celsius = (5.0/9.0) * (fahr-32.0);
 printf("%3.0f %6.1f\n", fahr, celsius); //se agragaron espacios para mas prolijidad
