fe07df548f50528232ba6ba274facccba8900195
Pieris128 <elpichichi2048@gmail.com>
2022-01-19 20:32:15 -0300

    guardo

---
+++ src/Libro C/Capitulo 1/1.2.VariablesYExpresionesAritmeticas/Conversor2.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/* imprime la tabla Fahrenheit-Celsius
+para fahr = 0, 20, ..., 300; versión de punto flotante */
+int main(){
+float fahr, celsius; //se usa float para mas exactitud
+int lower, upper, step;
+lower = 0 ; /* límite superior de la tabla de temperaturas */
+upper = 300; /* límite superior */
+step = 20 ; /* tamaño del incremento */
+fahr = lower;
+printf("Tabla de conversion Farenheit a Celsius\n");
+while (fahr <= upper) {
+celsius = (5.0/9.0) * (fahr-32.0);
+printf("%3.0f %6.1f\n", fahr, celsius); //se agragaron espacios para mas prolijidad
+fahr = fahr + step;}
+return 0;
+}
