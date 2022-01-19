fc4cedaed7eec973c093f2d4c74080f8079d69a6
Pieris128 <elpichichi2048@gmail.com>
2022-01-19 19:51:52 -0300

    primer codigo del libro c cap 1

---
+++ src/Libro C/Capitulo 1/1.Holamundo/holamundo.c
@@ -0,0 +1,7 @@
+# include <stdio.h> //incluye información acerca de la biblioteca estándar
+main( )                   // define una función llamada main
+                                 //   que no recibe valores de argumentos
+{                                  //  las proposiciones de main están encerradas entre llaves
+printf("hola, mundo\n");              // main llama a la función de biblioteca printf
+return 0;
+}                                     //          para escribir esta secuencia de caracteres;
