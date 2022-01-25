20f434b1995dd47bd5504fde0d1465a411c49ad5
Pieris128 <elpichichi2048@gmail.com>
2022-01-25 20:02:25 -0300

    arrays

---
+++ src/C/Makigas/1x07.ArraysyVLAs/arrays.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int i, edades [8];
+    for(i=0;i<8;i++){
+     printf("Introduce la edad %d: \n",i+1);
+     scanf("%d",&edades[i]);
+     }
+    return 0;
+
+
+}
